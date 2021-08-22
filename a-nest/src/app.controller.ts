324b186f457224dfbc2b5a4d5c938e2bee1533e9
8471919 <7471919@naver.com>
2021-08-22 20:51:10 +0900

    chore:morgan middleware 생성

--- a-nest/src/app.controller.ts
+++ a-nest/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('abc')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('hello')
+  getUser(): string {
+    return this.appService.getUser();
+  }
+  @Post('hi')
+  postUser(): string {
+    return this.appService.postUser();
   }
 }
