7d3a5a82b2c1767e58b49c1dde4ae2282ce1bce4
8471919 <7471919@naver.com>
2021-08-23 23:24:13 +0900

    chore: controller기초작업

--- a-nest/src/app.module.ts
+++ a-nest/src/app.module.ts
@@ -6,12 +6,15 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
+import { ChannelsController } from './channels/channels.controller';
+import { ChannelsService } from './channels/channels.service';
 import { DmsModule } from './dms/dms.module';
+import { ChannelsModule } from './channels/channels.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule, WorkspacesModule, ChannelsModule, DmsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ChannelsController],
+  providers: [AppService, ChannelsService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): any {
