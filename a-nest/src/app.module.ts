605de7c24410bf0f11dcdf400ef619fa02d0f06e
8471919 <7471919@naver.com>
2021-08-26 19:51:47 +0900

    chore: app.module에서 typeormmodule import

--- a-nest/src/app.module.ts
+++ a-nest/src/app.module.ts
@@ -9,14 +9,33 @@ import { ChannelsModule } from './channels/channels.module';
 import { ChannelsController } from './channels/channels.controller';
 import { ChannelsService } from './channels/channels.service';
 import { DmsModule } from './dms/dms.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UsersModule,
     WorkspacesModule,
     ChannelsModule,
     DmsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: ['entities/*.js'], //아예 하나씩 다 import해도됨
+      // autoLoadEntities: true, //버그가 있음. 직접 entity를 임포트하자.
+      synchronize: true, //개발환경일 때만(그때도 처음 돌릴때만 true) true, data가 손실될 수 있다.
+      logging: true, //개발할 때 query문을 볼 수 있게 하기위해 로깅을 true로 둔다.
+      // migrations: [__dirname + "src/migrations/*.ts"],
+      // cli: { migrationDir: 'src/migrations'},
+      // charset: 'utf8mb4',
+      keepConnectionAlive: true, //서버재시작(핫리로딩사용시)시 typeorm이 db연결을 유지하게 하기 위해서 true로 해둔다.
+    }),
   ],
   controllers: [AppController, ChannelsController],
   providers: [AppService, ChannelsService],
